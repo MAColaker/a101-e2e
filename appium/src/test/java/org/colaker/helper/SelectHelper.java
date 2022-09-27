74239d68787a3d90be7bd4003a6d0e041e0e25bb
Muhammed Ali ÇOLAKER <m.alicolaker@gmail.com>
2022-09-27 22:33:42 +0300

    update helper

--- appium/src/test/java/org/colaker/helper/SelectHelper.java
+++ appium/src/test/java/org/colaker/helper/SelectHelper.java
@@ -2,13 +2,24 @@ package org.colaker.helper;
 
 import org.colaker.base.BaseHelper;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.WebElement;
 
 public class SelectHelper extends BaseHelper {
+    VisibleHelper visibleHelper = new VisibleHelper();
+
     public void selectByVisibleText(By by, String text) {
         logger.info("'{}' elementi aranıyor", by);
-        Select select = new Select(waitHelper.untilElementClickable(by));
+        WebElement spinner = waitHelper.untilElementClickable(by);
+        spinner.click();
+
+        By elementOptionBy = By.xpath("//android.widget.ListView/android.widget.CheckedTextView[@text='" + text + "']");
+        logger.info("Ekran kaydırılacak");
+        while (!visibleHelper.isDisplayed(elementOptionBy)) {
+            logger.info("Ekran yukarı kaydırıldı");
+            verticalSwipeByPercentages(0.7, 0.2, 0.5);
+        }
+        WebElement element = waitHelper.untilElementClickable(elementOptionBy);
         logger.info("'{}' select objesinden '{}' değeri seçiliyor", by, text);
-        select.selectByVisibleText(text);
+        element.click();
     }
 }
--- appium/src/test/java/org/colaker/helper/VisibleHelper.java
+++ appium/src/test/java/org/colaker/helper/VisibleHelper.java
@@ -2,14 +2,19 @@ package org.colaker.helper;
 
 import org.colaker.base.BaseHelper;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 
 public class VisibleHelper extends BaseHelper {
     public boolean isElementVisible(By by) {
         return waitHelper.untilElementVisible(by) != null;
     }
 
-    public boolean isDisplayed(By by){
-        return driver.findElement(by).isDisplayed();
+    public boolean isDisplayed(By by) {
+        try {
+            return driver.findElement(by).isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 
     public boolean waitUntilElemenDisappear(By by) {
